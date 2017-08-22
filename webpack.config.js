a61fdd77ac58da578cde28d5af1bc2f77a2765ec
Tim Fogarty <timothy.fogo@gmail.com>
2017-08-22 16:53:29 -0700

    Switch to babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const debug = process.env.NODE_ENV !== "production";
 const path = require('path');
 const webpack = require("webpack");
 
@@ -15,7 +14,7 @@ module.exports = {
         exclude: [/node_modules/],
         use: {
           loader: 'babel-loader',
-          options: { presets: ['es2015', 'stage-0', 'react'] }
+          options: { presets: ['env', 'react'] }
         }
       }
     ]
