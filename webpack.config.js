b462f1683b2b22b0d2d93a3bfa3f5711e1b16ac8
Tim Fogarty <timothy.fogo@gmail.com>
2017-09-06 16:31:28 -0700

    Re-add stage-0 preset in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: [/node_modules/],
         use: {
           loader: 'babel-loader',
-          options: { presets: ['env', 'react'] }
+          options: { presets: ['stage-0', 'env', 'react'] }
         }
       }
     ]
