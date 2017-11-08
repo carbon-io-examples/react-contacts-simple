7fb09aab2ff42d66bf2d4a2d83eb399fcad7b62d
Tim Fogarty <timothy.fogo@gmail.com>
2017-11-08 10:39:43 -0800

    Add index import to client

--- src/js/client.js
+++ src/js/client.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import App from "./components/App"
 import store from './store'
 import { Provider } from 'react-redux'
+import '../index.html'
 import '../styles/main.css'
 
 ReactDOM.render(
